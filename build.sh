b3c0b6a6aae175122b7a32c59f20aefcf723e34f
chrisadamsonmcri <chris.adamson@mcri.edu.au>
2019-09-13 14:11:35 +1000

    Added detection of OS. Disable FLANN for centos. Fixed cmake VTK dir for centos. Use cmake3 in centos, cmake in ubuntu

--- build.sh
+++ build.sh
@@ -2,7 +2,25 @@
 
 MCRIBSDIR=`pwd`
 
-if [ ! -f "$MCRIBSDIR/VTK/VTK-install/lib/cmake/vtk-8.2/VTKConfig.cmake" ]
+. /etc/os-release
+
+BUILDTYPE=Release
+
+case $ID in
+	ubuntu)
+		CMAKE=cmake
+		VTKDEPENDS=$MCRIBSDIR/VTK/VTK-install/lib/cmake/vtk-8.2 
+		USINGFLANN=ON
+	;;
+	centos)
+		CMAKE=cmake3
+		VTKDEPENDS=$MCRIBSDIR/VTK/VTK-install/lib64/cmake/vtk-8.2
+		USINGFLANN=OFF
+		# boost-devel tbb-devel cmake3
+	;;
+esac
+
+if [ ! -f "$VTKDEPENDS/VTKConfig.cmake" ]
 then
 	# get and build VTK
 	mkdir -p VTK
@@ -17,7 +35,7 @@ then
 	
 	# ubuntu 
 	#apt install -y libglu1-mesa-dev libxt-dev python-dev gcc g++
-	cmake -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/VTK/VTK-install -DCMAKE_BUILD_TYPE=Release -DVTK_LEGACY_SILENT=ON -DVTK_WRAP_PYTHON=ON ../VTK
+	$CMAKE -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/VTK/VTK-install -DCMAKE_BUILD_TYPE=$BUILDTYPE -DVTK_LEGACY_SILENT=ON -DVTK_WRAP_PYTHON=ON ../VTK
 	make -j`nproc`
 	make install
 	cd $MCRIBSDIR
@@ -27,6 +45,6 @@ fi
 rm -fr MIRTK/MIRTK-build
 mkdir -p MIRTK/MIRTK-build
 cd MIRTK/MIRTK-build
-cmake -DMODULE_Deformable=ON -DMODULE_Mapping=ON -DMODULE_PointSet=ON -DMODULE_Scripting=ON -DWITH_TBB=ON -DMODULE_DrawEM=ON -DWITH_VTK=ON -DDEPENDS_VTK_DIR=$MCRIBSDIR/VTK/VTK-install/lib/cmake/vtk-8.2 -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/MIRTK/MIRTK-install -DCMAKE_BUILD_TYPE=Release -DWITH_FLANN=ON ../MIRTK
+$CMAKE -DMODULE_Deformable=ON -DMODULE_Mapping=ON -DMODULE_PointSet=ON -DMODULE_Scripting=ON -DWITH_TBB=ON -DMODULE_DrawEM=ON -DWITH_VTK=ON -DDEPENDS_VTK_DIR=$VTKDEPENDS -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/MIRTK/MIRTK-install -DCMAKE_BUILD_TYPE=$BUILDTYPE -DWITH_FLANN=$USINGFLANN ../MIRTK
 make -j`nproc`
 make install
