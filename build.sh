988a14de591959d0e1e24877fb35032d1a1d8ee7
chris adamson <chris.adamson@mcri.edu.au>
2019-10-11 14:58:41 +1100

    Make lightweight VTK build.

--- build.sh
+++ build.sh
@@ -20,8 +20,37 @@ case $ID in
 	;;
 esac
 
-if [ ! -f "$MIRTKVTKDEPENDS/VTKConfig.cmake" ]
-then
+ARCHFLAGS=""
+
+while [ ! -z "$1" ]
+do
+	if [ "$1" == "-h" -o "$1" == '--help' ]
+	then
+		echo "<ENVIRONMENT VARIABLES> $0 [options]"
+		echo
+		echo -e "ENVIRONMENT VARIABLES"
+		echo -e "\tCC\tSets the C compiler."
+		echo -e "\tCXX\tSets the C++ compiler."
+		echo
+		echo "options"
+		echo -e "\t-h or --help\tThis help."
+		echo -e "\t-debug\t\tBuild VTK and MIRTK with Debug switches, Release if not given."
+		echo -e "\t-archnative\tUse -march=native and -mtune=native when building VTK and MIRTK."
+		exit
+	fi
+	if [ "$1" == "-archnative" ]
+	then
+		ARCHFLAGS="-DCMAKE_C_FLAGS=\"-march=native -mtune=native\" -DCMAKE_CXX_FLAGS=\"-march=native -mtune=native\""
+	fi
+	if [ "$1" == "-debug" ]
+	then
+		BUILDTYPE=Debug
+	fi
+	shift;
+done
+
+#if [ ! -f "$MIRTKVTKDEPENDS/VTKConfig.cmake" ]
+#then
 	# get and build VTK
 	mkdir -p VTK
 	#rm -fr VTK/VTK-install
@@ -32,7 +61,9 @@ then
 	cd ..
 
 	
-	#LIGHTWEIGHTPYTHON="-DVTK_WRAP_PYTHON:BOOL=ON -DVTK_Group_StandAlone:BOOL=OFF -DVTK_Group_Rendering:BOOL=OFF -DModule_vtkCommonColor:BOOL=ON -DModule_vtkCommonComputationalGeometry:BOOL=ON -DModule_vtkCommonDataModel:BOOL=ON -DModule_vtkCommonExecutionModel:BOOL=ON -DModule_vtkCommonMath:BOOL=ON -DModule_vtkCommonMisc:BOOL=ON -DModule_vtkCommonSystem:BOOL=ON -DModule_vtkCommonTransforms:BOOL=ON -DModule_vtkFiltersCore:BOOL=ON -DModule_vtkFiltersExtraction:BOOL=ON -DModule_vtkFiltersGeneral:BOOL=ON -DModule_vtkFiltersGeneric:BOOL=ON -DModule_vtkFiltersGeometry:BOOL=ON -DModule_vtkFiltersPython:BOOL=ON -DModule_vtkIOCore:BOOL=ON -DModule_vtkIOGeometry:BOOL=ON -DModule_vtkIOLegacy:BOOL=ON -DModule_vtkWrappingPythonCore:BOOL=ON -DModule_vtkIOXML:BOOL=ON -DModule_vtkIOXMLPoly:BOOL=ON"
+	LIGHTWEIGHTPYTHON="-DVTK_WRAP_PYTHON:BOOL=ON -DVTK_Group_StandAlone:BOOL=OFF -DVTK_Group_Rendering:BOOL=OFF -DModule_vtkCommonColor:BOOL=ON -DModule_vtkCommonComputationalGeometry:BOOL=ON -DModule_vtkCommonDataModel:BOOL=ON -DModule_vtkCommonExecutionModel:BOOL=ON -DModule_vtkCommonMath:BOOL=ON -DModule_vtkCommonMisc:BOOL=ON -DModule_vtkCommonSystem:BOOL=ON -DModule_vtkCommonTransforms:BOOL=ON -DModule_vtkFiltersCore:BOOL=ON -DModule_vtkFiltersExtraction:BOOL=ON -DModule_vtkFiltersGeneral:BOOL=ON -DModule_vtkFiltersGeneric:BOOL=ON -DModule_vtkFiltersGeometry:BOOL=ON -DModule_vtkFiltersPython:BOOL=ON -DModule_vtkIOCore:BOOL=ON -DModule_vtkIOGeometry:BOOL=ON -DModule_vtkIOLegacy:BOOL=ON -DModule_vtkWrappingPythonCore:BOOL=ON -DModule_vtkIOXML:BOOL=ON -DModule_vtkIOXMLPoly:BOOL=ON -DModule_vtkFiltersHybrid:BOOL=ON -DModule_vtkFiltersModeling:BOOL=ON -DModule_vtkImagingStencil:BOOL=ON -DModule_vtkIOPLY:BOOL=ON -DModule_vtkFiltersFlowPaths:BOOL=ON -DModule_vtkFiltersParallel:BOOL=ON"
+
+# MIRTK vtkCommonCore vtkCommonDataModel vtkCommonExecutionModel vtkFiltersCore vtkFiltersExtraction vtkFiltersFlowPaths vtkFiltersGeneral vtkFiltersGeometry vtkFiltersHybrid vtkFiltersModeling vtkImagingCore vtkImagingStencil vtkIOGeometry vtkIOLegacy vtkIOPLY vtkIOXML
 
 	#rm -fr VTK-python2-build
 	#mkdir -p VTK-python2-build
@@ -45,24 +76,27 @@ then
 	#make install
 	#cd ..
 	
-	#rm -fr VTK-build VTK-install
-	#mkdir -p VTK-build
+	rm -fr VTK-build VTK-install
+#rm -fr VTK-build VTK-install
+	mkdir -p VTK-build
 	cd VTK-build
 	#$CMAKE -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/VTK/VTK-install -DCMAKE_BUILD_TYPE=$BUILDTYPE -DVTK_LEGACY_SILENT=ON -DVTK_WRAP_PYTHON=ON -DVTK_PYTHON_VERSION=3 -DVTK_Group_Rendering:BOOL=OFF ../VTK
-	$CMAKE -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/VTK/VTK-install -DCMAKE_BUILD_TYPE=$BUILDTYPE -DVTK_LEGACY_SILENT=ON -DVTK_WRAP_PYTHON=ON -DVTK_PYTHON_VERSION=3 -DVTK_Group_Rendering:BOOL=OFF ../VTK
+#eval $CMAKE $ARCHFLAGS -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/VTK/VTK-install -DCMAKE_BUILD_TYPE=$BUILDTYPE -DVTK_LEGACY_SILENT=ON -DVTK_WRAP_PYTHON=ON -DVTK_PYTHON_VERSION=3 -DVTK_Group_Rendering:BOOL=OFF ../VTK
+    eval $CMAKE $ARCHFLAGS -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/VTK/VTK-install -DCMAKE_BUILD_TYPE=$BUILDTYPE -DVTK_LEGACY_SILENT=ON -DVTK_WRAP_PYTHON=ON -DVTK_PYTHON_VERSION=3 $LIGHTWEIGHTPYTHON ../VTK
+    
 	make -j`nproc`
 	make install
 	cd $MCRIBSDIR
 
-fi
+#fi
 #exit
 #apt install -y zlib1g-dev libboost-dev libeigen3-dev libflann-dev
 rm -fr MIRTK/MIRTK-build MIRTK/MIRTK-install
 mkdir -p MIRTK/MIRTK-build
 cd MIRTK/MIRTK-build
-$CMAKE -DMODULE_Deformable=ON -DMODULE_Mapping=ON -DMODULE_PointSet=ON -DMODULE_Scripting=ON -DWITH_TBB=ON -DMODULE_DrawEM=ON -DWITH_VTK=ON -DDEPENDS_VTK_DIR=$MIRTKVTKDEPENDS -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/MIRTK/MIRTK-install -DCMAKE_BUILD_TYPE=$BUILDTYPE -DWITH_FLANN=$USINGFLANN ../MIRTK
+eval $CMAKE $ARCHFLAGS -DMODULE_Deformable=ON -DMODULE_Mapping=ON -DMODULE_PointSet=ON -DMODULE_Scripting=ON -DWITH_TBB=ON -DMODULE_DrawEM=ON -DWITH_VTK=ON -DDEPENDS_VTK_DIR=$MIRTKVTKDEPENDS -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/MIRTK/MIRTK-install -DCMAKE_BUILD_TYPE=$BUILDTYPE -DWITH_FLANN=$USINGFLANN ../MIRTK
 make -j`nproc`
 make install
 
 # the python 2 directories can be deleted since 
-rm -fr VTK/VTK-build VTK/VTK-install/include
+#rm -fr VTK/VTK-build VTK/VTK-install/include MIRTK/MIRTK-build MIRTK/MIRTK-install/include
