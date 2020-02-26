8168fa742b0a7f9c5435498748e3c3400ea3efd9
chris adamson <chris.adamson@mcri.edu.au>
2020-02-26 11:53:50 +1100

    Corrected patch file name.

--- build.sh
+++ build.sh
@@ -53,45 +53,44 @@ cd ITK
 git clone https://github.com/InsightSoftwareConsortium/ITK.git
 cd ITK
 git checkout tags/v4.13.2
+cd ..
 mkdir -p ITK-build
+cd ITK-build
+pwd
 cmake -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/ITK/ITK-install ../ITK
 make -j`nproc`
 make install
 cd $MCRIBSDIR
 #exit
 
-#if [ ! -f "$MIRTKVTKDEPENDS/VTKConfig.cmake" ]
-#then
-	# get and build VTK
-	mkdir -p VTK
-	#rm -fr VTK/VTK-install VTK/VTK-build
-	cd VTK
-	git clone https://github.com/Kitware/VTK.git
-	cd VTK
-	git checkout tags/v8.1.2
-	cd ../..
-	PATCHFILE=lib/vtk_intersection_test.patch
-	patch -p0 -N --dry-run --silent < $PATCHFILE 2>/dev/null
-	if [ $? -eq 0 ];
-	then
-	    #apply the patch
-	    patch -p0 -N < $PATCHFILE
-	fi
-	cd VTK
+# get and build VTK
+mkdir -p VTK
+#rm -fr VTK/VTK-install VTK/VTK-build
+cd VTK
+git clone https://github.com/Kitware/VTK.git
+cd VTK
+git checkout tags/v8.1.2
+cd ../..
+PATCHFILE=lib/vtk-8.1_intersection_test.patch
+patch -p0 -N --dry-run --silent < $PATCHFILE 2>/dev/null
+if [ $? -eq 0 ];
+then
+    #apply the patch
+    patch -p0 -N < $PATCHFILE
+fi
+cd VTK
 
-	LIGHTWEIGHTPYTHON="-DVTK_WRAP_PYTHON:BOOL=ON -DVTK_Group_StandAlone:BOOL=OFF -DVTK_Group_Rendering:BOOL=OFF -DModule_vtkCommonColor:BOOL=ON -DModule_vtkCommonComputationalGeometry:BOOL=ON -DModule_vtkCommonDataModel:BOOL=ON -DModule_vtkCommonExecutionModel:BOOL=ON -DModule_vtkCommonMath:BOOL=ON -DModule_vtkCommonMisc:BOOL=ON -DModule_vtkCommonSystem:BOOL=ON -DModule_vtkCommonTransforms:BOOL=ON -DModule_vtkFiltersCore:BOOL=ON -DModule_vtkFiltersExtraction:BOOL=ON -DModule_vtkFiltersGeneral:BOOL=ON -DModule_vtkFiltersGeneric:BOOL=ON -DModule_vtkFiltersGeometry:BOOL=ON -DModule_vtkFiltersPython:BOOL=ON -DModule_vtkIOCore:BOOL=ON -DModule_vtkIOGeometry:BOOL=ON -DModule_vtkIOLegacy:BOOL=ON -DModule_vtkWrappingPythonCore:BOOL=ON -DModule_vtkIOXML:BOOL=ON -DModule_vtkFiltersHybrid:BOOL=ON -DModule_vtkFiltersModeling:BOOL=ON -DModule_vtkImagingStencil:BOOL=ON -DModule_vtkIOPLY:BOOL=ON -DModule_vtkFiltersFlowPaths:BOOL=ON -DModule_vtkFiltersParallel:BOOL=ON"
+LIGHTWEIGHTPYTHON="-DVTK_WRAP_PYTHON:BOOL=ON -DVTK_Group_StandAlone:BOOL=OFF -DVTK_Group_Rendering:BOOL=OFF -DModule_vtkCommonColor:BOOL=ON -DModule_vtkCommonComputationalGeometry:BOOL=ON -DModule_vtkCommonDataModel:BOOL=ON -DModule_vtkCommonExecutionModel:BOOL=ON -DModule_vtkCommonMath:BOOL=ON -DModule_vtkCommonMisc:BOOL=ON -DModule_vtkCommonSystem:BOOL=ON -DModule_vtkCommonTransforms:BOOL=ON -DModule_vtkFiltersCore:BOOL=ON -DModule_vtkFiltersExtraction:BOOL=ON -DModule_vtkFiltersGeneral:BOOL=ON -DModule_vtkFiltersGeneric:BOOL=ON -DModule_vtkFiltersGeometry:BOOL=ON -DModule_vtkFiltersPython:BOOL=ON -DModule_vtkIOCore:BOOL=ON -DModule_vtkIOGeometry:BOOL=ON -DModule_vtkIOLegacy:BOOL=ON -DModule_vtkWrappingPythonCore:BOOL=ON -DModule_vtkIOXML:BOOL=ON -DModule_vtkFiltersHybrid:BOOL=ON -DModule_vtkFiltersModeling:BOOL=ON -DModule_vtkImagingStencil:BOOL=ON -DModule_vtkIOPLY:BOOL=ON -DModule_vtkFiltersFlowPaths:BOOL=ON -DModule_vtkFiltersParallel:BOOL=ON"
 
-	rm -fr VTK-build VTK-install
-	mkdir -p VTK-build
-	cd VTK-build
-	eval $CMAKE $ARCHFLAGS -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/VTK/VTK-install -DCMAKE_BUILD_TYPE=$BUILDTYPE -DVTK_LEGACY_SILENT=ON -DVTK_WRAP_PYTHON=ON -DVTK_PYTHON_VERSION=3 $LIGHTWEIGHTPYTHON ../VTK
+rm -fr VTK-build VTK-install
+mkdir -p VTK-build
+cd VTK-build
+eval $CMAKE $ARCHFLAGS -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$MCRIBSDIR/VTK/VTK-install -DCMAKE_BUILD_TYPE=$BUILDTYPE -DVTK_LEGACY_SILENT=ON -DVTK_WRAP_PYTHON=ON -DVTK_PYTHON_VERSION=3 $LIGHTWEIGHTPYTHON ../VTK
 
-	make -j`nproc`
-	make install
-	cd $MCRIBSDIR
+make -j`nproc`
+make install
+cd $MCRIBSDIR
 
-
-#fi
 #exit
 #apt install -y zlib1g-dev libboost-dev libeigen3-dev libflann-dev
 
