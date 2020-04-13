e2d7b32d0df2124ee3880b5cb2c4eff0f34d5649
chris.adamson <chris.adamson@mcri.edu.au>
2020-04-13 16:23:23 +1000

    Added T1 option for Deformable.

--- SetUpMCRIBS.sh
+++ SetUpMCRIBS.sh
@@ -2,4 +2,4 @@ export MCRIBS_HOME=`dirname $BASH_SOURCE`
 echo "MCRIBS_HOME set to $MCRIBS_HOME"
 
 export PATH=$PATH:${MCRIBS_HOME}/bin
-#export PYTHONPATH=$PYTHONPATH:${MCRIBS_HOME}/lib/python
+export PYTHONPATH=$PYTHONPATH:${MCRIBS_HOME}/lib/python
